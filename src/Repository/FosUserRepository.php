0e32121261ed9d59527267367cadc2cf03586786
AITS\nambinina2 <fetra.harinjatovo@axian-group.com>
2019-08-21 14:10:57 +0300

    configuraiton

---
+++ src/Repository/FosUserRepository.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nambinina2
+ * Date: 21/08/2019
+ * Time: 12:03
+ */
+
+namespace App\Repository;
+
+
+use Doctrine\ORM\EntityRepository;
+
+class FosUserRepository extends EntityRepository
+{
+
+}
