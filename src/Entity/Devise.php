6c9fa60eccb9d0549f7c47d25b289f2c1193c828
Christian Thierry git config --global user.mail thierry.moa.1410@gmail.com <thierry.moa.1410@gmail.com>
2019-09-02 10:08:17 +0300

    gestion des devises: ASSSUIVILITIGE-26 : liste ASSSUIVILITIGE-25 : Ajout modification supprimer

---
+++ src/Entity/Devise.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\DeviseRepository")
+ */
+class Devise
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=5)
+     */
+    private $code;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $libelle;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isActif=true;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCode(): ?string
+    {
+        return $this->code;
+    }
+
+    public function setCode(string $code): self
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    public function getLibelle(): ?string
+    {
+        return $this->libelle;
+    }
+
+    public function setLibelle(string $libelle): self
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    public function getIsActif(): ?bool
+    {
+        return $this->isActif;
+    }
+
+    public function setIsActif(bool $isActif): self
+    {
+        $this->isActif = $isActif;
+
+        return $this;
+    }
+}
