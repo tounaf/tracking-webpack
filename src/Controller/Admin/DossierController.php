5d451024b992d460f3fa0a91e337f0d994c06890
AITS\nambinina2 <fetra.harinjatovo@axian-group.com>
2019-09-13 12:08:55 +0300

    edition

--- src/Controller/Admin/DossierController.php
+++ src/Controller/Admin/DossierController.php
@@ -5,8 +5,12 @@ namespace App\Controller\Admin;
 use App\Entity\Dossier;
 use App\Form\DossierType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Translation\TranslatorInterface;
 
 /**
  * Class DossierController
@@ -16,6 +20,56 @@ use Symfony\Component\Routing\Annotation\Route;
 class DossierController extends Controller
 {
 
+    /**
+     * @var TranslatorInterface
+     */
+    private $trans;
+
+    /**
+     * @var Session|SessionInterface
+     */
+    private $session;
+
+    /**
+     * DossierController constructor.
+     * @param TranslatorInterface $translator
+     * @param SessionInterface $sessionBag
+     */
+    public function __construct(TranslatorInterface $translator, SessionInterface $sessionBag)
+    {
+        $this->trans = $translator;
+        $this->session = $sessionBag;
+    }
+
+    /**
+     * @Route("/dossier/render/create",name="render_create_dossier")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function renderCreateDossier()
+    {
+        return $this->render('dossier/dossier.html.twig');
+    }
+
+    /**
+     *
+     * @Route("/dossier/render/edit/{id}", name="render_edit_dossier")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function renderEditDossier(Dossier $dossier = null, Request $request)
+    {
+        $id = $request->get('id');
+        if ($dossier) {
+            $form = $this->createForm(DossierType::class, $dossier, array(
+                'method' => 'POST',
+                'action' => $this->generateUrl('edit_dossier', array(
+                    'id' => $id
+                ))
+            ));
+        }
+        return $this->render('dossier/dossier.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
     /**
      *
      * @Route("/dossier/create", name="create_dossier", methods={"GET","POST"})
@@ -36,14 +90,47 @@ class DossierController extends Controller
             $echeance = $request->get('dossier')['echeance'];
             $alert = $request->get('dossier')['alerteDate'];
             $dossier->setAlerteDate(new \DateTime($alert))->setDateLitige(new \DateTime($dateLitige))->setEcheance(new \DateTime($echeance));
-//            dump($dossier);die;
-            $em->persist($dossier);
-            $em->flush();
+            try {
+                $em->persist($dossier);
+                $em->flush();
+                $this->session->getFlashBag()->add('success',$this->trans->trans('label.create.success'));
+            } catch (\Exception $exception) {
+                $this->session->getFlashBag()->add('danger',$this->trans->trans('label.create.error'));
+            }
+
+            return $this->redirectToRoute('render_edit_dossier', array('id' =>$dossier->getId()));
         }
 
-        return $this->render('dossier/form.html.twig', array(
-            'form' => $form->createView()
-        ));
+       return $this->redirectToRoute('render_create_dossier');
 
+    }
+
+    /**
+     * @Route("/dossier/edit/{id}", name="edit_dossier", methods={"GET", "POST"})
+     * @param Dossier $dossier
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function editDossier(Dossier $dossier = null, Request $request)
+    {
+        $id = $request->get('id');
+        if ($dossier instanceof Dossier) {
+            $form = $this->createForm(DossierType::class, $dossier, array(
+                'method' => 'POST',
+                'action' => 'edit_dossier'
+            ))->handleRequest($request)
+            ;
+            $response = new JsonResponse();
+            if ($form->isSubmitted()) {
+                try {
+
+                    $this->getDoctrine()->getManager()->flush();
+                    $this->session->getFlashBag()->add('success', $this->trans->trans('label.edit.success'));
+
+                } catch (\Exception $exception) {
+                    $this->session->getFlashBag()->add('danger', $this->trans->trans('label.edit.error'));
+                }
+            }
+        }
+        return $this->redirectToRoute('render_edit_dossier', array('id' => $id));
     }
 }
