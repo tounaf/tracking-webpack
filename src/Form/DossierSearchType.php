ec5de34be1609161ff376427b4ec6ca6a43450c7
AITS\nambinina2 <fetra.harinjatovo@axian-group.com>
2019-09-16 17:13:39 +0300

    TRANSLATION LABEL

--- src/Form/DossierSearchType.php
+++ src/Form/DossierSearchType.php
@@ -5,17 +5,34 @@ namespace App\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Translation\TranslatorInterface;
 
 class DossierSearchType extends AbstractType
 {
+    private $trans;
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->trans = $translator;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nom')
-            ->add('reference')
-            ->add('categorie')
-            ->add('entite')
-            ->add('statut')
+            ->add('nom', null,array(
+                'label' => $this->trans->trans('NOM')
+            ))
+            ->add('reference', null, array(
+                'label' => $this->trans->trans('RÉFÉRENCE')
+            ))
+            ->add('categorie', null, array(
+                'label' => $this->trans->trans('CATÉGORIE')
+            ))
+            ->add('entite', null, array(
+                'label' => $this->trans->trans('ENTITÉ')
+            ))
+            ->add('statut', null, array(
+                'label' => $this->trans->trans('STATUT')
+            ))
         ;
     }
 
