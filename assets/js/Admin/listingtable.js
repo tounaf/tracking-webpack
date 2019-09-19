382b37907aba55376205bfe42dce18e3c6f8f294
johnny <johnny.rakotoarison@axian-group.com>
2019-09-19 14:54:28 +0300

    Maj js pour datable

--- assets/js/Admin/listingtable.js
+++ assets/js/Admin/listingtable.js
@@ -258,4 +258,231 @@ $(document).ready(function () {
           { "width": "6%", "targets": 2, orderable: false},
         ]
     });
+    
+    // dataTable pour liste type de prestation
+    $('#listTypePrestation').DataTable({
+        searching : true,
+        destroy : true,
+        bLengthChange: false,
+        info: false,
+        language: {
+            processing:     "Traitement en cours...",
+            search:         "Rechercher&nbsp;:",
+            lengthMenu:     "Afficher _MENU_ &eacute;l&eacute;ments",
+            info:           "Affichage de l'&eacute;lement _START_ &agrave; _END_ sur _TOTAL_ &eacute;l&eacute;ments",
+            infoEmpty:      "Affichage de l'&eacute;lement 0 &agrave; 0 sur 0 &eacute;l&eacute;ments",
+            infoFiltered:   "(filtr&eacute; de _MAX_ &eacute;l&eacute;ments au total)",
+            infoPostFix:    "",
+            loadingRecords: "Chargement en cours...",
+            zeroRecords:    "Aucun &eacute;l&eacute;ment &agrave; trouv&eacute;",
+            emptyTable:     "Aucune donnée disponible dans le tableau",
+            paginate: {
+                first:      "Premier",
+                previous:   "Pr&eacute;c&eacute;dent",
+                next:       "Suivant",
+                last:       "Dernier"
+            },
+            aria: {
+                sortAscending:  ": activer pour trier la colonne par ordre croissant",
+                sortDescending: ": activer pour trier la colonne par ordre décroissant"
+            }
+        },
+        "autoWidth": false,
+        responsive: true,
+        columnDefs: [
+          { "width": "45%", "targets": 0, orderable: true },
+          { "width": "45%", "targets": 1, orderable: true},
+          { "width": "6%", "targets": 2, orderable: false},
+        ]
+    });
+
+    // dataTable pour liste devise
+    $('#listDevise').DataTable({
+        searching : true,
+        destroy : true,
+        bLengthChange: false,
+        info: false,
+        language: {
+            processing:     "Traitement en cours...",
+            search:         "Rechercher&nbsp;:",
+            lengthMenu:     "Afficher _MENU_ &eacute;l&eacute;ments",
+            info:           "Affichage de l'&eacute;lement _START_ &agrave; _END_ sur _TOTAL_ &eacute;l&eacute;ments",
+            infoEmpty:      "Affichage de l'&eacute;lement 0 &agrave; 0 sur 0 &eacute;l&eacute;ments",
+            infoFiltered:   "(filtr&eacute; de _MAX_ &eacute;l&eacute;ments au total)",
+            infoPostFix:    "",
+            loadingRecords: "Chargement en cours...",
+            zeroRecords:    "Aucun &eacute;l&eacute;ment &agrave; trouv&eacute;",
+            emptyTable:     "Aucune donnée disponible dans le tableau",
+            paginate: {
+                first:      "Premier",
+                previous:   "Pr&eacute;c&eacute;dent",
+                next:       "Suivant",
+                last:       "Dernier"
+            },
+            aria: {
+                sortAscending:  ": activer pour trier la colonne par ordre croissant",
+                sortDescending: ": activer pour trier la colonne par ordre décroissant"
+            }
+        },
+        "autoWidth": false,
+        responsive: true,
+        columnDefs: [
+          { "width": "30%", "targets": 0, orderable: true },
+          { "width": "30%", "targets": 1, orderable: true},
+          { "width": "30%", "targets": 2, orderable: false},
+          { "width": "6%", "targets": 3, orderable: false},
+        ]
+    });
+
+    
+    // dataTable pour liste devise
+    $('#listEtapeSuivante').DataTable({
+        searching : true,
+        destroy : true,
+        bLengthChange: false,
+        info: false,
+        language: {
+            processing:     "Traitement en cours...",
+            search:         "Rechercher&nbsp;:",
+            lengthMenu:     "Afficher _MENU_ &eacute;l&eacute;ments",
+            info:           "Affichage de l'&eacute;lement _START_ &agrave; _END_ sur _TOTAL_ &eacute;l&eacute;ments",
+            infoEmpty:      "Affichage de l'&eacute;lement 0 &agrave; 0 sur 0 &eacute;l&eacute;ments",
+            infoFiltered:   "(filtr&eacute; de _MAX_ &eacute;l&eacute;ments au total)",
+            infoPostFix:    "",
+            loadingRecords: "Chargement en cours...",
+            zeroRecords:    "Aucun &eacute;l&eacute;ment &agrave; trouv&eacute;",
+            emptyTable:     "Aucune donnée disponible dans le tableau",
+            paginate: {
+                first:      "Premier",
+                previous:   "Pr&eacute;c&eacute;dent",
+                next:       "Suivant",
+                last:       "Dernier"
+            },
+            aria: {
+                sortAscending:  ": activer pour trier la colonne par ordre croissant",
+                sortDescending: ": activer pour trier la colonne par ordre décroissant"
+            }
+        },
+        "autoWidth": false,
+        responsive: true,
+        columnDefs: [
+          { "width": "45%", "targets": 0, orderable: true },
+          { "width": "45%", "targets": 1, orderable: true},
+          { "width": "6%", "targets": 2, orderable: false},
+        ]
+    });
+
+    // dataTable pour liste nature décision
+    $('#listNatureDecision').DataTable({
+        searching : true,
+        destroy : true,
+        bLengthChange: false,
+        info: false,
+        language: {
+            processing:     "Traitement en cours...",
+            search:         "Rechercher&nbsp;:",
+            lengthMenu:     "Afficher _MENU_ &eacute;l&eacute;ments",
+            info:           "Affichage de l'&eacute;lement _START_ &agrave; _END_ sur _TOTAL_ &eacute;l&eacute;ments",
+            infoEmpty:      "Affichage de l'&eacute;lement 0 &agrave; 0 sur 0 &eacute;l&eacute;ments",
+            infoFiltered:   "(filtr&eacute; de _MAX_ &eacute;l&eacute;ments au total)",
+            infoPostFix:    "",
+            loadingRecords: "Chargement en cours...",
+            zeroRecords:    "Aucun &eacute;l&eacute;ment &agrave; trouv&eacute;",
+            emptyTable:     "Aucune donnée disponible dans le tableau",
+            paginate: {
+                first:      "Premier",
+                previous:   "Pr&eacute;c&eacute;dent",
+                next:       "Suivant",
+                last:       "Dernier"
+            },
+            aria: {
+                sortAscending:  ": activer pour trier la colonne par ordre croissant",
+                sortDescending: ": activer pour trier la colonne par ordre décroissant"
+            }
+        },
+        "autoWidth": false,
+        responsive: true,
+        columnDefs: [
+          { "width": "45%", "targets": 0, orderable: true },
+          { "width": "45%", "targets": 1, orderable: true},
+          { "width": "6%", "targets": 2, orderable: false},
+        ]
+    });
+
+    
+    // dataTable pour liste niveau décision
+    $('#listNiveauDecision').DataTable({
+        searching : true,
+        destroy : true,
+        bLengthChange: false,
+        info: false,
+        language: {
+            processing:     "Traitement en cours...",
+            search:         "Rechercher&nbsp;:",
+            lengthMenu:     "Afficher _MENU_ &eacute;l&eacute;ments",
+            info:           "Affichage de l'&eacute;lement _START_ &agrave; _END_ sur _TOTAL_ &eacute;l&eacute;ments",
+            infoEmpty:      "Affichage de l'&eacute;lement 0 &agrave; 0 sur 0 &eacute;l&eacute;ments",
+            infoFiltered:   "(filtr&eacute; de _MAX_ &eacute;l&eacute;ments au total)",
+            infoPostFix:    "",
+            loadingRecords: "Chargement en cours...",
+            zeroRecords:    "Aucun &eacute;l&eacute;ment &agrave; trouv&eacute;",
+            emptyTable:     "Aucune donnée disponible dans le tableau",
+            paginate: {
+                first:      "Premier",
+                previous:   "Pr&eacute;c&eacute;dent",
+                next:       "Suivant",
+                last:       "Dernier"
+            },
+            aria: {
+                sortAscending:  ": activer pour trier la colonne par ordre croissant",
+                sortDescending: ": activer pour trier la colonne par ordre décroissant"
+            }
+        },
+        "autoWidth": false,
+        responsive: true,
+        columnDefs: [
+          { "width": "45%", "targets": 0, orderable: true },
+          { "width": "45%", "targets": 1, orderable: true},
+          { "width": "6%", "targets": 2, orderable: false},
+        ]
+    });
+
+
+    
+    // dataTable pour liste information pièce jointe
+    $('#listInformationPj').DataTable({
+        searching : true,
+        destroy : true,
+        bLengthChange: false,
+        info: false,
+        language: {
+            processing:     "Traitement en cours...",
+            search:         "Rechercher&nbsp;:",
+            lengthMenu:     "Afficher _MENU_ &eacute;l&eacute;ments",
+            info:           "Affichage de l'&eacute;lement _START_ &agrave; _END_ sur _TOTAL_ &eacute;l&eacute;ments",
+            infoEmpty:      "Affichage de l'&eacute;lement 0 &agrave; 0 sur 0 &eacute;l&eacute;ments",
+            infoFiltered:   "(filtr&eacute; de _MAX_ &eacute;l&eacute;ments au total)",
+            infoPostFix:    "",
+            loadingRecords: "Chargement en cours...",
+            zeroRecords:    "Aucun &eacute;l&eacute;ment &agrave; trouv&eacute;",
+            emptyTable:     "Aucune donnée disponible dans le tableau",
+            paginate: {
+                first:      "Premier",
+                previous:   "Pr&eacute;c&eacute;dent",
+                next:       "Suivant",
+                last:       "Dernier"
+            },
+            aria: {
+                sortAscending:  ": activer pour trier la colonne par ordre croissant",
+                sortDescending: ": activer pour trier la colonne par ordre décroissant"
+            }
+        },
+        "autoWidth": false,
+        responsive: true,
+        columnDefs: [
+          { "width": "45%", "targets": 0, orderable: true },
+          { "width": "45%", "targets": 1, orderable: true},
+          { "width": "6%", "targets": 2, orderable: false},
+        ]
+    });
 });
