5f17c6b747d22edd154bbbb4213b52aa8e04ec0d
AITS\nambinina2 <fetra.harinjatovo@axian-group.com>
2019-09-24 17:30:48 +0300

    resolve serialized sub object

--- src/EventListner/HistorySubscriber.php
+++ src/EventListner/HistorySubscriber.php
@@ -11,22 +11,27 @@ namespace App\EventListner;
 
 use App\Entity\Dossier;
 use App\Entity\History;
+use Doctrine\Common\Annotations\AnnotationReader;
 use Doctrine\Common\EventSubscriber;
-use Doctrine\Common\EventArgs;
-use Doctrine\ORM\Event\OnFlushEventArgs;
 use Doctrine\ORM\Event\PreUpdateEventArgs;
-use JMS\Serializer\SerializerInterface;
 use Symfony\Component\DependencyInjection\Container;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Mapping\Factory\ClassMetadataFactory;
+use Symfony\Component\Serializer\Mapping\Loader\AnnotationLoader;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer;
 
 class HistorySubscriber implements EventSubscriber
 {
     private $serializer;
     private $container;
-    public function __construct(Container $container)
+    private $security;
+    public function __construct(Container $container, Security $security)
     {
         $this->container = $container;
         $this->serializer = $container->get('serializer');
+        $this->security = $security;
     }
 
     public function getSubscribedEvents()
@@ -49,16 +54,23 @@ class HistorySubscriber implements EventSubscriber
                 return;
             }
             $changesets = $unitOfWork->getEntityChangeSet($entity);
-            // serialize data changesets
-            $dataserialize = $this->serializer->serialize($changesets, 'json');
 
+            $classMetadataFactory = new ClassMetadataFactory(new AnnotationLoader(new AnnotationReader()));
+            $encoder = new JsonEncoder();
+
+            $normalizer = new ObjectNormalizer($classMetadataFactory);
+            $serializer = new Serializer([$normalizer], [$encoder]);
+
+            // serialize data changesets
+            $data = $serializer->normalize($changesets,null,['groups'=> ['groupe1']]);
+            $dataserialize = $serializer->serialize($data, 'json');
             //get class name
             $nameClass = $em->getMetadataFactory()->getMetadataFor(get_class($entity))->getName();
             $nameWithOutNameSpace = join('', array_slice(explode('\\', $nameClass), -1));
-
             $history = new History();
             $history->setClasseName($nameWithOutNameSpace);
             $history->setMetadata($dataserialize);
+            $history->setUser($this->security->getToken()->getUser());
             $history->setDossier($entity);
             $em->persist($history);
             $unitOfWork->computeChangeSets();
